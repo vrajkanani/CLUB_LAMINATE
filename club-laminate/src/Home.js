05f5027a0bb6f18a2084942e008135f41d2812ef
vrajkanani <116797973+vrajkanani@users.noreply.github.com>
2025-04-20 13:00:51 +0530

    Update Home.js

--- club-laminate/src/Home.js
+++ club-laminate/src/Home.js
@@ -1,70 +1,67 @@
 import React from "react";
 import "./Home.css";
 import ClubLouvers1 from "./product/ClubLouvers1";
-import ClubLouvers2 from "./product/ClubLouvers2";
-import CoziLouvers1 from "./product/CoziLouvers1";
-import CoziLouvers2 from "./product/CoziLouvers2";
 import Products from "./Product";
 
 const HomePage = () => {
   return (
     <>
-      <div className="container">
-        <div className="row">
-          <div className="col text-center text-success mb-2 p-5 ">
-            <h2>Our Product</h2>
+      <div className="container py-4">
+        {/* Hero section */}
+        <div className="row mb-4">
+          <div className="col-12 text-center">
+            <h1 className="section-title">Our Products</h1>
+            <p className="lead text-muted">Discover our premium collection of louvers</p>
           </div>
         </div>
-        <div className="row">
-          <div className="col mb-2 p-3">
-            <div className="gallery justify-content-start">
-              <img src="./images/home/img1.jpg" alt="asdf"></img>
-              <img src="./images/home/img2.jpg" alt="asdf"></img>
-              <img src="./images/home/img3.jpg" alt="asdf"></img>
-              <img src="./images/home/img4.jpg" alt="asdf"></img>
-              <img src="./images/home/img5.jpg" alt="asdf"></img>
-              <img src="./images/home/img8.jpg" alt="asdf"></img>
-              <img src="./images/home/img9.jpg" alt="asdf"></img>
-              <img src="./images/home/img10.jpg" alt="asdf"></img>
-              <img src="./images/home/img11.jpg" alt="asdf"></img>
-              <img src="./images/home/img12.jpg" alt="asdf"></img>
-              <img src="./images/home/img13.jpg" alt="asdf"></img>
-              <img src="./images/home/img15.jpg" alt="asdf"></img>
+
+        {/* Gallery section */}
+        <div className="row mb-4">
+          <div className="col-12 px-md-3 px-0">
+            <div className="gallery-container">
+              <div className="gallery">
+                <img src="./images/home/img1.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img2.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img3.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img4.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img5.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img8.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img9.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img10.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img11.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img12.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img13.jpg" alt="Louver product" className="img-fluid"/>
+                <img src="./images/home/img15.jpg" alt="Louver product" className="img-fluid"/>
+              </div>
             </div>
           </div>
         </div>
+        
+        {/* API Products section */}
+        <div className="row mb-4">
+          <div className="col-12 px-md-3 px-0">
+            <div className="product-section">
+              <div className="text-center mb-3">
+                <h2 className="section-title">Featured Products</h2>
+              </div>
+              <div className="product-container">
+                <Products />
+              </div>
+            </div>
+          </div>
+        </div>
+        
+        {/* Club Louvers 1 section */}
         <div className="row">
-          <div className="col mb-2 p-5">
-            <div className="row">
-              <div className="col text-center text-success mb-2 p-5 ">
-                <h2>from api</h2>
+          <div className="col-12 px-md-3 px-0">
+            <div className="product-section">
+              <div className="text-center mb-3">
+                <h2 className="section-title">Club Louvers Collection</h2>
+              </div>
+              <div className="product-container">
+                <ClubLouvers1 />
               </div>
             </div>
-            <Products />
-            <div className="row">
-              <div className="col text-center text-success mb-2 p-5 ">
-                <h2>Club Louvers 1</h2>
-              </div>
-            </div>
-            <ClubLouvers1 />
-            <div className="row">
-              <div className="col text-center text-success mb-2 p-5 ">
-                <h2>Club Louvers 2</h2>
-              </div>
-            </div>
-            <ClubLouvers2 />
-            <div className="row">
-              <div className="col text-center text-success mb-2 p-5 ">
-                <h2>Cozi Louvers 1</h2>
-              </div>
-            </div>
-            <CoziLouvers1 />
-            <div className="row">
-              <div className="col text-center text-success mb-2 p-5 ">
-                <h2>Cozi Louvers 2</h2>
-              </div>
-            </div>
-            <CoziLouvers2 />
           </div>
         </div>
       </div>
