ce45190ec74e3c6d71c35e8df6867b21bdc60e3d
vrajkanani <116797973+vrajkanani@users.noreply.github.com>
2025-04-20 12:58:31 +0530

    Update App.js

--- club-laminate/src/App.js
+++ club-laminate/src/App.js
@@ -15,9 +15,6 @@ import AddProduct from "./AddProduct.js";
 import EditProduct from "./EditProduct.js"
 import Product from "./Product.js";
 import ClubLouvers1 from "./product/ClubLouvers1";
-import ClubLouvers2 from "./product/ClubLouvers2";
-import CoziLouvers1 from "./product/CoziLouvers1";
-import CoziLouvers2 from "./product/CoziLouvers2";
 
 function App() {
   return (
@@ -37,9 +34,6 @@ function App() {
           <Route path="/ContactUsPage" element={<ContactUsPage />}></Route>
           <Route path="/ServicesPage" element={<Services />}></Route>
           <Route path="/1 club louvers" element={<ClubLouvers1 />}></Route>
-          <Route path="/2 club louvers" element={<ClubLouvers2 />}></Route>
-          <Route path="/1 Cozi louvers" element={<CoziLouvers1 />}></Route>
-          <Route path="/2 Cozi louvers" element={<CoziLouvers2 />}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
