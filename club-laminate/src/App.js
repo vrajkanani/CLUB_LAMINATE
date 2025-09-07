dbc858a25b6466b6aba5a3a357f14a64dc7f6dac
vrajkanani <116797973+vrajkanani@users.noreply.github.com>
2025-09-07 11:00:04 +0530

    Update App.js

--- club-laminate/src/App.js
+++ club-laminate/src/App.js
@@ -1,46 +1,3 @@
-// import React from "react";
-// import "./App.css";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Layout from "./Layout";
-// import Home from "./Home";
-// import AboutUsPage from "./AboutUsPage";
-// import ContactUsPage from "./ContactUsPage";
-// import Services from "./Services.js";
-// import LoginPage from "./LoginPage.js";
-// import BookNow from "./BookNow";
-// import Panding from "./panding";
-// import Conform from "./Conform";
-// import FeedBack from "./feedback.js";
-// import AddProduct from "./AddProduct.js";
-// import EditProduct from "./EditProduct.js"
-// import Product from "./Product.js";
-// import ClubLouvers1 from "./product/ClubLouvers1";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Layout />}>
-//           <Route index element={<Home />}></Route>
-//           <Route path="/login" element={<LoginPage />}></Route>
-//           <Route path="/BookNow" element={<BookNow />}></Route>
-//           <Route path="/PandingOrders" element={<Panding />}></Route>
-//           <Route path="/ConformOrders" element={<Conform />}></Route>
-//           <Route path="/feedback" element={<FeedBack />}></Route>
-//           <Route path="/addproduct" element={<AddProduct />}></Route>
-//           <Route path="/editproduct/:id" element={<EditProduct />}></Route>
-//           <Route path="/Product" element={<Product />}></Route>
-//           <Route path="/AboutUsPage" element={<AboutUsPage />}></Route>
-//           <Route path="/ContactUsPage" element={<ContactUsPage />}></Route>
-//           <Route path="/ServicesPage" element={<Services />}></Route>
-//           <Route path="/1 club louvers" element={<ClubLouvers1 />}></Route>
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
 import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -58,6 +15,7 @@ import AddProduct from "./AddProduct.js";
 import EditProduct from "./EditProduct.js";
 import Product from "./Product.js";
 import ClubLouvers1 from "./product/ClubLouvers1";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   useEffect(() => {
@@ -85,6 +43,7 @@ function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
