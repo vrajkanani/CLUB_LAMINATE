2eff8510e02568a137680bbbe3ee381cc897755a
vrajkanani <116797973+vrajkanani@users.noreply.github.com>
2025-09-25 10:32:01 +0530

    Update Product.js

--- club-laminate/src/Product.js
+++ club-laminate/src/Product.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Product.css";
 
 function Products() {
@@ -90,3 +90,4 @@ function Products() {
 }
 
 export default Products;
+
